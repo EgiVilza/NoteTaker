2086f657218b320376003a84376e00efc51e0722
EgiVilza <egivilza@gmail.com>
2021-02-07 15:34:14 -0600

    Added app.delete code

--- Develop/server.js
+++ Develop/server.js
@@ -19,30 +19,55 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 app.get(`/api/notes`, (req, res) => res.sendFile(path.join(__dirname, "/db/db.json")))
 
 app.post(`/api/notes`, (req, res) => {
-    let readFile = JSON.parse(fs.readFileSync("./db/db.json", "utf8")) 
+    let getFile = JSON.parse(fs.readFileSync("./db/db.json", "utf8")) 
     let newNote = req.body
-    let noteID = readFile.length
-    let noteIDString = noteID.toString()
-    newNote.id = noteIDString
-    readFile.push(newNote)
+    let noteID = getFile.length
+    newNote.id = noteID
+    getFile.push(newNote)
 
-    fs.writeFile("./db/db.json", JSON.stringify(readFile), (err) => {
+    fs.writeFile("./db/db.json", JSON.stringify(getFile), (err) => {
         if (err) {
             console.log(err)
         }
     })
 
     console.log("Writing to the file worked")
-    res.json(readFile)
+    res.json(getFile)
 })
 
 app.delete(`/api/notes/:id`, (req, res) => {
+    //Grabs the object array data from the json from and puts it into a variable
+    let getFile = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
 
+    //Grabs the id
+    let noteID = req.params.id
+
+    //Deletes the element in the getFile array
+    getFile.splice(noteID, 1)
+
+    //Calls the new ElementID Function
+    newElementID()
+    
+    //Once a element gets deleted, ID get reassigned
+    function newElementID() {
+        //This is to recreate the new ID for each element on the object array
+        let newID = 0
+
+        getFile.forEach(element => {
+            element.id = newID
+            newID++
+        });
+    }
+
+    fs.writeFile("./db/db.json", JSON.stringify(getFile), (err) => {
+        if (err) {
+            console.log(err)
+        }
+    })
+
+    console.log("Note Deleted")
+    res.json(getFile)
 })
-
-//[{"title":"Test Title","text":"Test text"}]
-
-
 
 //Starts Server
 app.listen(PORT, function() {
